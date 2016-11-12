93b8e671acbef796aeadcbb68d31b2d824e948fc
ira2016 <gvir2015@mail.ru>
2016-11-12 22:16:33 +0200

    homework1

---
+++ homeworks/Irina.Glushneva_ira2016/Homework1/test2.py
@@ -0,0 +1,11 @@
+name = input("What is your name?")
+lname = input("What is your last name?")
+age = int(input("What year were you born?"))
+city = input("Whatciti were you born?")
+work = input ("Your place of work?")
+print('Name and surname:' + name+ ' ' +lname)
+print(age)
+print('You citi born:' + city)
+print('You place of work:' + work)
+
+
